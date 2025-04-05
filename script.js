98d88dcf0527317561db0ccb43002cb4b405e2c4
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-05 15:54:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
+	str = str.trim();
+	let i = 0;
+	let j = str.length-1;
 
+	while(i<j){
+		if(str[i] != str[j]){
+			return false;
+		}
+		i = i+1;
+		j = j-1;
+	}
+	return true;
 }
 module.exports = palindrome
