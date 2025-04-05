82e3275937b91fdf769e73083bd08c5d68f9740e
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-05 15:58:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-	str = str.trim();
-	let i = 0;
-	let j = str.length-1;
-
-	while(i<=j){
-		if(str[i] != str[j]){
-			return false;
-		}
-		i = i+1;
-		j = j-1;
-	}
-	return true;
+	str = str.toLowerCase().replace(/\W/g,'')
+    let newStr = str.split('').reverse().join('')
+    console.log(newStr)
+    console.log(str)
+    if(newStr == str){
+        return true;
+    }
+    
+    return false;
 }
 module.exports = palindrome
